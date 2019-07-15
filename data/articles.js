80d0b3cf67b72cd8f9b1a8fce58e85b5088f9bce
eankol <eankol@163.com>
2019-07-15 15:36:03 +0800

    up v1.0

---
+++ data/articles.js
@@ -0,0 +1,6 @@
+var articles = [{
+    title: "前端进阶之路（web）",
+    date: "2019-07-15",
+    author: "Ean",
+    inner: ""
+}]
--- js/startApp.js
+++ js/startApp.js
@@ -2,31 +2,33 @@ window.onload = function() {
     //加载内容；
     new elms({
         data: {
-            hello: "this is elms!",
-            test: "hi,",
-            shows: "（双击查看）",
-            aa: Math.random()
+            // hello: "this is elms!",
+            // test: "hi,",
+            // shows: "（双击查看）",
+            // aa: Math.random()
         },
-        elements: [{
-            el: document.getElementById("app"),
-            actions: [{
-                action: 'click',
-            }]
-        }, {
-            el: document.getElementById("22"),
-            actions: [{
-                action: 'click',
-                test: '$.tests(\'5\')'
-            }]
-        }],
+        elements: [
+            //     {
+            //     el: document.getElementById("app"),
+            //     actions: [{
+            //         action: 'click',
+            //     }]
+            // }, {
+            //     el: document.getElementById("22"),
+            //     actions: [{
+            //         action: 'click',
+            //         test: '$.tests(\'5\')'
+            //     }]
+            // }
+        ],
         methods: {
-            tests(a) {
-                console.log(a)
-            },
-            show() {
-                $.tests('this is a test msg')
-                $.shows = "什么都做不了！"
-            }
+            // tests(a) {
+            //     console.log(a)
+            // },
+            // show() {
+            //     $.tests('this is a test msg')
+            //     $.shows = "什么都做不了！"
+            // }
         }
     })
 }
