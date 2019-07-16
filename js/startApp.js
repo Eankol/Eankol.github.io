67e2ed9d72cc4227209a671a06d85ec33d7af215
eankol <eankol@163.com>
2019-07-16 14:44:43 +0800

    a1

--- js/startApp.js
+++ js/startApp.js
@@ -1,33 +1,4 @@
 window.onload = function() {
     //加载内容；
-    new elms({
-        data: {
-            inner: "",
-            // test: "hi,",
-            // shows: "（双击查看）",
-            // aa: Math.random()
-        },
-        elements: [{
-                el: document.getElementById("app"),
-                actions: [{
-                    action: 'click',
-                }]
-            } //,{
-            //     el: document.getElementById("22"),
-            //     actions: [{
-            //         action: 'click',
-            //         test: '$.tests(\'5\')'
-            //     }]
-            // }
-        ],
-        methods: {
-            // tests(a) {
-            //     console.log(a)
-            // },
-            // show() {
-            //     $.tests('this is a test msg')
-            //     $.shows = "什么都做不了！"
-            // }
-        }
-    })
+
 }
